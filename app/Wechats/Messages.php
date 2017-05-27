60969cb5e828045f0ea0c6d02d4d0c86e36e4149
Bro Qiang <broqiang@qq.com>
2017-05-27 23:25:17 +0800

    测试消息回复

--- app/Wechats/Messages.php
+++ app/Wechats/Messages.php
@@ -1,10 +1,22 @@
 <?php
 namespace App\Wechats;
 
+use EasyWeChat\Message\Text;
+
 class Messages
 {
+
+    private $text;
+
+    public function __construct(Text $text)
+    {
+        $this->text = text;
+    }
+
     public function messageHandler($message)
     {
-
+        if ($message->MsgType == 'text') {
+            $text = new Text(['content' => '你好，测试信息']);
+        }
     }
 }
