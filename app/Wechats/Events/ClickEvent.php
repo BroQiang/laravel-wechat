287fc459187cef4382cbbfb49d741c9a42d29fbe
Bro Qiang <broqiang@qq.com>
2017-05-28 00:59:06 +0800

    修改点击事件

--- app/Wechats/Events/ClickEvent.php
+++ app/Wechats/Events/ClickEvent.php
@@ -6,7 +6,7 @@ class ClickEvent
     protected $message;
 
     /**
-     * 定义Key对应的类
+     * 定义Key对应的类，暂时先不处理了，等有空再处理
      */
     protected $keys = [
         'activity_push_poster' => 'App\\Wechats\\Activities\\Posters',
@@ -19,6 +19,12 @@ class ClickEvent
 
     public function clickHandler()
     {
-        $key = $this->message->EventKey;
+        // 暂时只有海报活动一个点击事件，时间又紧，就先写死了，等有空再处理
+        $keyword = 'activity_push_poster';
+        $len = strlen($keyword);
+
+        if(strncmp($keyword,substr($this->message->EventKey, 0,$len)) == 0) {
+            new Text(['content' => '你点击了菜单']);
+        }
     }
 }
