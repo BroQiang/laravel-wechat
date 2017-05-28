c439dcc4ca796d5f37b34cc628b76fc5b16ed7ee
Bro Qiang <broqiang@qq.com>
2017-05-28 21:46:26 +0800

    Update

--- app/Repositories/PosterRepository.php
+++ app/Repositories/PosterRepository.php
@@ -13,6 +13,7 @@ class PosterRepository
         $material = app('wechat')->material;
         
         foreach ($medias as $media) {
+            dd($media);
             // 删除所有海报对应的缓存的素材（微信中）
             $material->delete($media->media_id);
             // 删除海报对应的缓存media_id
--- app/Wechats/Event.php
+++ app/Wechats/Event.php
@@ -20,14 +20,19 @@ class Event
         switch ($this->message->Event) {
             case 'subscribe': // 关注
                 return (new SubscribeEvent($this->message))->subscribeHandler();
+                break;
             case 'unsubscribe': //取消关注
                 return (new UnsubscribeEvent($this->message))->unsubscribeHandler();
+                break;
             case 'SCAN': //已经关注
                 return (new ScanEvent($this->message))->scanHandler();
+                break;
             case 'CLICK': //自定义菜单
                 return (new ClickEvent($this->message))->clickHandler();
+                break;
             default:
                 return null;
+                break;
         }
     }
 
