f633475268202b1960c1fc8415de2521d04c239b
Bro Qiang <broqiang@qq.com>
2017-05-29 14:43:31 +0800

    修改清除缓存的顺序

--- app/Repositories/PosterRepository.php
+++ app/Repositories/PosterRepository.php
@@ -12,10 +12,10 @@ class PosterRepository
         $material = app('wechat')->material;
 
         foreach ($medias as $media) {
-            // 删除所有海报对应的缓存的素材（微信中）
-            $material->delete($media->media_id);
             // 删除海报对应的缓存media_id
             $media->delete();
+            // 删除所有海报对应的缓存的素材（微信中）
+            $material->delete($media->media_id);
         }
     }
 
